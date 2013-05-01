a149fe99b0a4dcab35453539a22fdc215b9c15eb
Renjith I S <mailrenjithis@gmail.com>
2013-05-02 05:10:08 +0530

    Cleaned up imports. Removed unused imports and removed some commented code

--- src/in/renjithis/xposed/mods/ussdfilter/USSDFilter.java
+++ src/in/renjithis/xposed/mods/ussdfilter/USSDFilter.java
@@ -1,68 +1,17 @@
 package in.renjithis.xposed.mods.ussdfilter;
 
+import java.lang.reflect.Method;
+import android.widget.Toast;
+
+// Imports for XposedBridge
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
-//Imports for PhoneUtils class. Need to cleanup and remove those which are not needed
-
-
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.app.ProgressDialog;
-//import android.bluetooth.IBluetoothHeadsetPhone;
-import android.content.ActivityNotFoundException;
+//Imports for PhoneUtils class
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager;
-import android.content.res.Configuration;
-import android.graphics.drawable.Drawable;
-import android.media.AudioManager;
-import android.net.Uri;
-import android.net.sip.SipManager;
-//import android.os.AsyncResult;
-import android.os.Handler;
-import android.os.Message;
-import android.os.RemoteException;
-//import android.os.SystemProperties;
-import android.preference.PreferenceManager;
-import android.provider.Settings;
-import android.telephony.PhoneNumberUtils;
-import android.text.TextUtils;
-import android.util.Log;
-import android.view.KeyEvent;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.WindowManager;
-import android.widget.EditText;
-import android.widget.Toast;
-
-//import com.android.internal.telephony.Call;
-//import com.android.internal.telephony.CallManager;
-//import com.android.internal.telephony.CallStateException;
-//import com.android.internal.telephony.CallerInfo;
-//import com.android.internal.telephony.CallerInfoAsyncQuery;
-//import com.android.internal.telephony.Connection;
-//import com.android.internal.telephony.MmiCode;
-//import com.android.internal.telephony.Phone;
-//import com.android.internal.telephony.PhoneConstants;
-//import com.android.internal.telephony.TelephonyCapabilities;
-//import com.android.internal.telephony.TelephonyProperties;
-//import com.android.internal.telephony.cdma.CdmaConnection;
-//import com.android.internal.telephony.sip.SipPhone;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Hashtable;
-import java.util.Iterator;
-import java.util.List;
-
-import java.lang.reflect.Method;
 
 public class USSDFilter implements IXposedHookLoadPackage {
 
@@ -73,9 +22,7 @@ public class USSDFilter implements IXposedHookLoadPackage {
         	return;
         
         XposedBridge.log("Found phone app");
-        
-        //todo
-        
+                
     	findAndHookMethod("com.android.phone.PhoneUtils", 
     			lpparam.classLoader, 
     			"displayMMIComplete", 
@@ -92,13 +39,11 @@ public class USSDFilter implements IXposedHookLoadPackage {
 		    			
 		    			Context context=(Context) param.args[1];
 		    			Object mmiCode=param.args[2];
-//		    			Class MmiCode = mmiCode.getClass();
-		    			XposedBridge.log("mmicode type = "+mmiCode.toString());
+//		    			XposedBridge.log("mmicode type = "+mmiCode.toString());
 		    			
 		    			String filterString="free GPRS";
 		    			
 		    			Method getMessageMethod=mmiCode.getClass().getDeclaredMethod("getMessage", null);
-//		    			String text = context.getText(R.string.puk_unlocked);
 		    			String text = (String) getMessageMethod.invoke(mmiCode, null);
 		    			XposedBridge.log("text="+text);
 		    			if(text.contains(filterString))
