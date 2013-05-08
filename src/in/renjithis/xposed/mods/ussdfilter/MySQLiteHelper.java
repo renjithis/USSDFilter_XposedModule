212fb7824045dbb2aa4549ca22db6ba073ba34cb
Renjith I S <mailrenjithis@gmail.com>
2013-05-08 09:50:07 +0530

    Added Log table to database need to add table management class

--- src/in/renjithis/xposed/mods/ussdfilter/MySQLiteHelper.java
+++ src/in/renjithis/xposed/mods/ussdfilter/MySQLiteHelper.java
@@ -7,7 +7,7 @@ import android.util.Log;
 
 public class MySQLiteHelper extends SQLiteOpenHelper {
 
-  public static final String TABLE_FILTERS = "preferences";
+  public static final String TABLE_FILTERS = "filters";
   public static final String COLUMN_ID = "_id";
   
   public static final String COLUMN_FILTER_NAME = "filter_name";
@@ -16,17 +16,31 @@ public class MySQLiteHelper extends SQLiteOpenHelper {
   public static final String COLUMN_FILTER_OUTPUT_TYPE = "filter_output";
   public static final String COLUMN_FILTER_PRIORITY = "filter_priority";
   public static final String COLUMN_FILTER_ENABLED = "filter_enabled";
-
+  
+  public static final String TABLE_LOG = "log";
+//  public static final String COLUMN_ID = "_id";
+  
+  public static final String COLUMN_LOG_MESSAGE = "log_message";
+  public static final String COLUMN_LOG_STATUS = "log_status";
+  public static final String COLUMN_LOG_DATE = "log_date";
+  
   private static final String DATABASE_NAME = "ussdfilter.db";
   private static final int DATABASE_VERSION = 1;
 
   // Database creation sql statement
-  private static final String DATABASE_CREATE = "create table "
-      + TABLE_FILTERS + "(" + COLUMN_ID
-      + " integer primary key autoincrement, " + COLUMN_FILTER_NAME
-      + " text not null, " + COLUMN_FILTER_TYPE + " integer, " + COLUMN_FILTER_STRING 
-      + " text not null, " + COLUMN_FILTER_OUTPUT_TYPE + "integer, " 
-      + COLUMN_FILTER_PRIORITY + "integer, " + COLUMN_FILTER_ENABLED + "integer);";
+  private static final String DATABASE_CREATE_TABLE_FILTERS = "create table "
+		  + TABLE_FILTERS + "(" + COLUMN_ID + " integer primary key autoincrement, " 
+		  + COLUMN_FILTER_NAME + " text not null, " 
+		  + COLUMN_FILTER_TYPE + " integer, " 
+		  + COLUMN_FILTER_STRING + " text not null, " 
+		  + COLUMN_FILTER_OUTPUT_TYPE + "integer, " 
+		  + COLUMN_FILTER_PRIORITY + "integer, " 
+		  + COLUMN_FILTER_ENABLED + "integer);";
+  private static final String DATABASE_CREATE_TABLE_LOG = "create talbe "
+		  + TABLE_LOG + "(" + COLUMN_ID + "integer primary key autoincrement, "
+		  + COLUMN_LOG_MESSAGE + "text not null, " 
+		  + COLUMN_LOG_STATUS + "integer, "
+		  + COLUMN_LOG_DATE + "text not null);";
 
   public MySQLiteHelper(Context context) {
     super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -34,7 +48,8 @@ public class MySQLiteHelper extends SQLiteOpenHelper {
 
   @Override
   public void onCreate(SQLiteDatabase database) {
-    database.execSQL(DATABASE_CREATE);
+    database.execSQL(DATABASE_CREATE_TABLE_FILTERS);
+    database.execSQL(DATABASE_CREATE_TABLE_LOG);
   }
 
   @Override
