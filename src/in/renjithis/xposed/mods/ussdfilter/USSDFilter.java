09897b349cf6be322281254f6c57f5f25836144f
Renjith I S <mailrenjithis@gmail.com>
2013-05-19 20:38:43 +0530

    Removed some commented code

--- src/in/renjithis/xposed/mods/ussdfilter/USSDFilter.java
+++ src/in/renjithis/xposed/mods/ussdfilter/USSDFilter.java
@@ -143,56 +143,7 @@ public class USSDFilter implements IXposedHookLoadPackage {
 		
 		return filterList;
 	}
-	
-//	private String readFile(String fileName) {
-//		// check if external storage (sdcard/user accessible internal storage) is avaiable
-//		// need to switch to internal storage
-//		boolean mExternalStorageAvailable = false;
-//		boolean mExternalStorageReadable = false;
-//		boolean mExternalStorageWriteable = false;
-//		String state = Environment.getExternalStorageState();
-//
-//		if (Environment.MEDIA_MOUNTED.equals(state)) {
-//			// We can read and write the media
-//			mExternalStorageAvailable = mExternalStorageReadable = mExternalStorageWriteable = true;
-//		} else if (Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
-//			// We can only read the media
-//			mExternalStorageAvailable = true;
-//			mExternalStorageReadable = true;
-//			mExternalStorageWriteable = false;
-//		} else {
-//			// Something else is wrong. It may be one of many other states, but all we need
-//			//  to know is we can neither read nor write
-//			mExternalStorageAvailable = mExternalStorageReadable = mExternalStorageWriteable = false;
-//		}
-//
-//		if(!mExternalStorageReadable)
-//		{
-//			myLog("External strorage not readable");
-//			return null;
-//		}
-//		
-//		File textFile = new File(Environment.getExternalStorageDirectory(), fileName);
-//		if(!textFile.exists() || !textFile.canRead())
-//		{
-//			myLog("Unable to read file:" + textFile.getPath());
-//			return null;
-//		}
-//		String content = null;
-//		//			File file = new File(file); //for ex foo.txt
-//		try {
-//			FileReader reader = new FileReader(textFile);
-//			char[] chars = new char[(int) textFile.length()];
-//			reader.read(chars);
-//			content = new String(chars);
-//			reader.close();
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-//		return content;
-//	}
-	
-	
+
 	private void showNotification(Context context, String title, String contentText) {
 		NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context)
 		        .setSmallIcon(R.drawable.ic_launcher)
@@ -204,20 +155,6 @@ public class USSDFilter implements IXposedHookLoadPackage {
 		NotificationManager mNotificationManager =
 		    (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 		mNotificationManager.notify(0, mBuilder.build());
-		
-		
-		// Alternate implementation
-		
-//		// Build notification
-//		Notification notification = new Notification.Builder(context)
-//		        .setContentTitle("New mail from " + "test@gmail.com")
-//		        .setContentText("Subject")
-//		        .setSmallIcon(R.drawable.icon);
-//		
-//		// Hide the notification after its selected
-//		notification.flags |= Notification.FLAG_AUTO_CANCEL;
-//		        
-//		mNotificationManager.notify(0, notification); 
 	}
 	 
 }
